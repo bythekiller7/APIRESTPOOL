451d258b03e4ebe7ce754a4267696c552ee6194e
byronc377 <Byronc3601@hotmail.com>
2024-01-07 01:22:08 -0600

    primer push

--- server.py
+++ server.py
@@ -1,9 +1,16 @@
 import os
 from flask import Flask, send_from_directory, render_template, redirect
-
+from app import app as producto_app  # Importando la app de app.py
+from flask import Blueprint
 app = Flask(__name__)
 
 port = int(os.environ.get("PORT", 5000))
+
+
+productos_blueprint = Blueprint('productos', __name__, url_prefix='/productos')  # Blueprint para la app de app.py
+
+app.register_blueprint(productos_blueprint)  # Registrando el Blueprint
+
 
 @app.route('/static/<path:path>')
 def serve_static(path):
@@ -13,9 +20,6 @@ def serve_static(path):
 def home():
    return render_template('index.html')
 
-@app.route('/<path:path>')
-def all_routes(path):
-    return redirect('/')
 
 if __name__ == "__main__":
     app.run(port=port)
